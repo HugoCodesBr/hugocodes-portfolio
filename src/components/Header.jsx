76953adf641dde0aa399a560844bd64835275ead
Hugo Silva <hugo.silva@indexdigital.com.br>
2023-04-29 11:17:50 -0300

    Adicionar segunda parte do menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import '../sass/components/header.scss';
 import LightHLogo from '../assets/hugocodes-horizontal-light.png';
+import HorizontalLogo from '../assets/hugocodes-horizontal.png';
 import ThemeChanger from './ThemeChanger';
 import MenuContent from './Menu/MenuContent';
 import { useEffect, useRef, useState } from 'react';
@@ -7,18 +8,43 @@ import MenuBurger from './Menu/MenuBurger';
 
 const Header = ({ changeTheme, darkTheme }) => {
   const [menuOpen, setMenuOpen] = useState(false);
+  const [scrollActive, setScrollActive] = useState(false);
 
   function burgerActive() {
     setMenuOpen(!menuOpen);
   }
 
+  useEffect(() => {
+    function handleScroll() {
+      const scrollPosition = window.scrollY;
+      console.log('rolou: ' + scrollPosition);
+
+      if (scrollPosition > 1) {
+        setScrollActive(true);
+      } else {
+        setScrollActive(false);
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
-    <header className={`header`}>
+    <header
+      className={`header ${scrollActive ? 'change-color' : ''} ${
+        darkTheme ? 'dark' : ''
+      }`}
+    >
       <div className="container">
         <div className="header__content">
           <div className="header__logo">
             <a href="#">
-              <img src={LightHLogo} alt="" />
+              <img
+                src={scrollActive && !darkTheme ? HorizontalLogo : LightHLogo}
+                alt=""
+              />
             </a>
           </div>
           <div className="header__menu">
